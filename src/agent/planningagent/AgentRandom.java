54adb3e7e6cbc1cbe9561db43bdfc55684b47b38
Bruno Buiret <bruno.b@apps-panel.com>
2016-03-07 16:34:48 +0100

    Formatage des sources

--- src/agent/planningagent/AgentRandom.java
+++ src/agent/planningagent/AgentRandom.java
@@ -1,6 +1,5 @@
 package agent.planningagent;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,56 +7,49 @@ import environnement.Action;
 import environnement.Etat;
 import environnement.MDP;
 import environnement.gridworld.ActionGridworld;
+
 /**
- * Cet agent choisit une action aleatoire parmi toutes les autorisees dans chaque etat
+ * Cet agent choisit une action aleatoire parmi toutes les autorisees dans
+ * chaque etat
+ *
  * @author lmatignon
  *
  */
-public class AgentRandom extends PlanningValueAgent{
-	
-	
-	public AgentRandom(MDP _m) {
-		super(_m);
-	}
+public class AgentRandom extends PlanningValueAgent {
 
-	@Override
-	public Action getAction(Etat e) {
-		//*** VOTRE CODE
-		
-		
-		
-		return null;
-		
-	}
+    public AgentRandom(MDP _m) {
+        super(_m);
+    }
 
-	
-	
-	@Override
-	public double getValeur(Etat _e) {
-		return 0.0;
-	}
+    @Override
+    public Action getAction(Etat e) {
+        //*** VOTRE CODE
 
-	
+        return null;
 
-	@Override
-	public List<Action> getPolitique(Etat _e) {
-		//*** VOTRE CODE
-		
-		return null;
-	}
+    }
 
-	@Override
-	public void updateV() {
-		System.out.println("l'agent random ne planifie pas");
-	}
+    @Override
+    public double getValeur(Etat _e) {
+        return 0.0;
+    }
 
-	@Override
-	public void setGamma(double parseDouble) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public List<Action> getPolitique(Etat _e) {
+        //*** VOTRE CODE
 
+        return null;
+    }
 
+    @Override
+    public void updateV() {
+        System.out.println("l'agent random ne planifie pas");
+    }
 
+    @Override
+    public void setGamma(double parseDouble) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
